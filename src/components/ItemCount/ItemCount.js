51c5178edc33fe2ae766566ebef2b0c25c9618ed
javimiglie <javimiglie@gmail.com>
2022-07-19 15:32:20 -0300

    agregando itemCount

---
+++ src/components/ItemCount/ItemCount.js
@@ -0,0 +1,30 @@
+import { useState } from 'react';
+import { Button } from '@mui/material';
+
+const ItemCount = () => {
+  const [count, setCount] = useState(1);
+  const [stock, setStock] = useState(5);
+
+  const addCount = () => {
+    if (count < 5) {
+      setCount(count + 1);
+    }
+  };
+  const restCount = () => {
+    if (count > 1) {
+      setCount(count - 1);
+    }
+  };
+  return (
+    <div className='container-comprar'>
+      <div className='button-item'>
+        <Button onClick={restCount}> - </Button>
+        <p> {count} </p>
+        <Button onClick={addCount}> + </Button>
+      </div>
+      <button> Comprar </button>
+    </div>
+  );
+};
+
+export default ItemCount;
